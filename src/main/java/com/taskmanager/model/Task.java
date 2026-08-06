98d8adf5e8f75d5ebf7f586123f8da95a3e8515d
henriqueoliveira-dev <henriquepantaleaodeoliveira@gmail.com>
2026-08-06 16:19:31 -0300

    feat: configure dependencies mysql connector dependency fest: create Task Class (attributes, constructors, getters and setters, toString method) feat: implement DatabaseConnection configuration

---
+++ src/main/java/com/taskmanager/model/Task.java
@@ -0,0 +1,80 @@
+package com.taskmanager.model;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private Long id;
+    private String title;
+    private String description;
+    private  boolean completed;
+    private LocalDateTime createdAt;
+
+    public Task() {
+    }
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Task(Long id, String title, String description, boolean completed, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", completed=" + completed +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
